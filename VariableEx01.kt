93a20ff098bf0c6b32c3051ddf77588b391ebe24
Byungil Oh <dhquddlf5@gmail.com>
2025-01-14 10:53:08 +0900

    [feat] : 입출력 및 변수 관련 예제 추가

---
+++ VariableEx01.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var data : Int =1234
+
+    println("data : " + data)
+    //string template
+    println("data : $data")
+    
+}
+
+// ➜  Kotlin git:(main) ✗ kotlinc VariableEx01.kt -include-runtime -d VariableEx01.jar
+// ➜  Kotlin git:(main) ✗ java -jar VariableEx01.jar 
